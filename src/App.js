bc926d597d57e45870c61b7e2a2f02629777de57
philip howley <howley.phil@gmail.com>
2022-03-12 11:17:24 -0500

    Add Basic app with 3 pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom"
+// shared
+import Footer from "./components/shared/Footer"
+import Header from "./components/shared/Header"
+import Navbar from "./components/shared/Navbar"
+// pages
+import HomePage from "./pages/Home"
+import AboutPage from "./pages/About"
+import ContactPage from "./pages/Contact"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import local data
+import { CAMPSITES } from "./data/campsites"
+
+export class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      campsites: CAMPSITES,
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <div className='container'>
+          <Header />
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/about' component={AboutPage} />
+            <Route exact path='/contact' component={ContactPage} />
+          </Switch>
+          {/* <p>Reviewing Week 3 - React</p>
+          {this.state.campsites.map((campsite) => (
+            <div>{campsite.name}</div>
+          ))} */}
+          <Footer />
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
